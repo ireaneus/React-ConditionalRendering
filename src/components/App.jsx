c6c2f115c5ae704e91718fe283671751846b2b43
Ireaneus <bwdave@protonmail.com>
2022-05-01 15:32:49 -0600

    React-Conditional-Rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
   );
 }
 
